09afe84ff4d4e729a3b3af17cb403e98d77c0037
alfonsodguez <alfonsoalejandro33@gmail.com>
2023-07-02 12:04:38 +0200

    setup project

--- public/js/checkPedidosStorage.js
+++ public/js/checkPedidosStorage.js
@@ -1,8 +1,8 @@
- /**
-* cada vez que haya un cambio en la url /Tienda/Productos/"xxxx" comprobamos
-* si existe datos del pedido en el localStorage para pintarlo en el layout
-*/
- $(function() {
+/**
+ * cada vez que haya un cambio en la url /Tienda/Productos/"xxxx" comprobamos
+ * si existe datos del pedido en el localStorage para pintarlo en el layout
+ */
+$(function() {
     const pedidoStorage = JSON.parse(localStorage.getItem('pedido'));
     if(pedidoStorage != null) {
         pedidoStorage.forEach(itemPedido => {
