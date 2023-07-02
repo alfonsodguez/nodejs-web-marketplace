526ba6f601c699d84c7e95d29032e1afea64577a
alfonsodguez <alfonsoalejandro33@gmail.com>
2023-07-02 11:54:36 +0200

    setup project

--- public/js/treeviewCategorias.js
+++ public/js/treeviewCategorias.js
@@ -4,13 +4,13 @@ const separadorCategoria  = "-"
 function despliegaSubcategorias() {
     const idCategoria    = $(this).parent().attr('id')                 
     const regexCategoria = new RegExp("^" + idCategoria + "-[0-9]+$")
-    const subCategorias  = categorias.filter(categoria => categoria.pathcategoria.search(regexCategoria) != -1)
+    const subCategorias  = categorias.filter(categoria => categoria.path.search(regexCategoria) != -1)
 
     if (subCategorias.length > 0) {
         let tags = '<ul class="nested">'
         
         subCategorias.forEach(subCategoria => { 
-            tags += `<li id="${subCategoria.pathcategoria}"><span class="caret">${subCategoria.categoria}</span></li>` 
+            tags += `<li id="${subCategoria.path}"><span class="caret">${subCategoria.nombre}</span></li>` 
         });
         tags += '</ul>'
 
@@ -29,9 +29,9 @@ function despliegaSubcategorias() {
 
 const categorias = JSON.parse(serializeCategorias.replace(/&quot;/g, `"`))  // <-- deserializar categorias 
 categorias.forEach(categoria => {
-    const paths = categoria.pathcategoria.split(separadorCategoria)
+    const paths = categoria.path.split(separadorCategoria)
     if (paths.length == 1) {
-        treeview.append(`<li id="${categoria.pathcategoria}"><span class="caret">${categoria.categoria}</span></li>`)          
+        treeview.append(`<li id="${categoria.path}"><span class="caret">${categoria.nombre}</span></li>`)          
     }
 })
 
